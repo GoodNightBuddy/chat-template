f59fa32b33e02157385600e720b00bb8129be208
Pavel <fun.love.nuts@gmail.com@>
2022-08-20 23:02:38 +0300

    Refactor chatBody

--- src/components/ChatBody/ChatBody.tsx
+++ src/components/ChatBody/ChatBody.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import "./ChatBody.scss"
 import UserIcon from "../UI/UserIcon/UserIcon";
 import { useParams } from "react-router-dom";
@@ -8,18 +7,19 @@ import { useAppSelector } from '../../store/store';
 
 const ChatBody = () => {
 
-  const [user, setUser] = useState({});
-  const [currentChatMessages, setCurrentChatMessages] = useState([]);
   const [users, messages] = useAppSelector(state => [state.user.users, state.message.messages]);
   const { id } = useParams();
 
-  useEffect(() => {
-    setUser(users.find(user => user.id === +id) || {});
-    const curMessages = messages.filter(m => m.chatId === +id);
-    curMessages.sort((a, b) => new Date(a.time) - new Date(b.time));
-    setCurrentChatMessages(curMessages);
-  }, [id, messages, users])
-  
+  const user = id ? users.find(user => user.id === +id) : null;
+  const curMessages = id ? messages.filter(m => m.chatId === +id) : [];
+  curMessages.sort((a, b) => new Date(a.time) > new Date(b.time) ? 1 : -1);
+
+
+  if(!user) {
+    return (
+      <div>Beda, user ne naiden...</div> 
+    )
+  }
 
   return (
     <div className="chat__body">
@@ -29,7 +29,7 @@ const ChatBody = () => {
       </div>
 
       <div className="chat__messagelist">
-        {currentChatMessages.map((e, i) => <Message message={e} userImage={user.photoURL} key={i} />)}
+        {curMessages.map((e, i) => <Message message={e} userImage={user.photoURL} key={i} />)}
       </div>
 
       <div className="chat__input">
