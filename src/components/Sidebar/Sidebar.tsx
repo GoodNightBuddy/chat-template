cfbcffbb8e92c212c0d24614f626c8d2ec93cb6a
Pavel <fun.love.nuts@gmail.com@>
2022-08-21 23:20:27 +0300

    Added user avatar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -7,10 +7,21 @@ import UserIcon from '../UI/UserIcon/UserIcon';
 import "./Sidebar.scss"
 
 const Sidebar = () => {
+
+  const defaultImage = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSf-_eFTxpPLfn16swgOup3z7QLLblNK_mq2Q&usqp=CAU"
   const [search, setSearch] = useState('');
   const [users, messages] = useAppSelector(state => [state.user.users, state.message.messages]);
 
   const auth = getAuth();
+  const user = auth.currentUser;
+
+  if (!user) {
+    return (
+      <div>Oops. something</div>
+    )
+  }
+
+  console.log(auth);
 
   const searchedUsers = (users.filter(u => u.name.toLowerCase().includes(search.toLowerCase())))
 
@@ -19,11 +30,6 @@ const Sidebar = () => {
   };
 
   const deleteHandler = () => {
-    const user = auth.currentUser;
-    if (!user) {
-      console.log('Oops, something went wrong -  no such user');
-      return
-    }
     deleteUser(user)
       .catch((error) => {
         console.log(error);
@@ -33,15 +39,18 @@ const Sidebar = () => {
   return (
     <aside className="sidebar">
       <div className="sidebar__header">
-        <span className='for-dropdown'>
-          <UserIcon
-            imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSf-_eFTxpPLfn16swgOup3z7QLLblNK_mq2Q&usqp=CAU"
-          />
-          <ul className="dropdown">
-            <li onClick={signOuntHandler}><i className="fa-solid fa-right-from-bracket"></i>Sign out</li>
-            <li onClick={deleteHandler}><i className="fa-solid fa-right-from-bracket"></i>Delete account</li>
-          </ul>
-        </span>
+        <div className='user-info'>
+          <div className='for-dropdown'>
+            <UserIcon
+              imgUrl={user?.photoURL || defaultImage}
+            />
+            <ul className="dropdown">
+              <li onClick={signOuntHandler}><i className="fa-solid fa-right-from-bracket"></i>Sign out</li>
+              <li onClick={deleteHandler}><i className="fa-solid fa-right-from-bracket"></i>Delete account</li>
+            </ul>
+          </div>
+          <div className='user-info__user-name'>{user?.displayName || user?.email}</div>
+        </div>
 
         <Search value={search} setValue={setSearch} />
       </div>
