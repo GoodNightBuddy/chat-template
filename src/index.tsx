43b68b0829ff1fb153590f7995e05e103f94a5ae
Pavel <fun.love.nuts@gmail.com@>
2022-08-22 14:07:11 +0300

    test commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './store/store';
 import { Provider } from 'react-redux';
 import './firebase/firebase-auth'
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
